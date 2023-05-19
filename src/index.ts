4c6abdcb175fd5368a2770a5bfe71522903180ac
Alexandre BREAULT <creepminecraft53@gmail.com>
2023-05-19 12:45:00 +0200

    ajouts import components

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { Cellule } from './component/Cellule/Cellule';
+import { Table } from './component/Table/table';
+import { TableBody } from './component/TableBody/TableBody';
+import { TableHeader } from './component/TableHeader/tableHeader';
+import { Entete } from './component/Entete/Entete';
+import { Ligne } from './component/Ligne/Ligne';
+
+export { Cellule, Table, TableBody, TableHeader, Entete, Ligne };
